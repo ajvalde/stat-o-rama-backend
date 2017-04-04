e7fb4ffb3b31bc6944657b85457e3c0e15ff7c1f
Anthony <anthonyguitarman13@gmail.com>
2017-04-04 11:08:12 -0600

    Runs locally

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/api', function(req,res){
     });
     
 });
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(5000);
 console.log('API is running on port 5000')
