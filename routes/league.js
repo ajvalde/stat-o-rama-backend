6cbdc3310888c4ce8dc82cbbf099f81cd1687661
Anthony <anthonyguitarman13@gmail.com>
2017-04-07 15:04:25 -0600

    getting LoL stats

--- routes/league.js
+++ routes/league.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const request = require('request');
 const knex = require('../db/knex.js');
 const lolkey = process.env.LoL_key
+const summoner_name = ""
+let summoner_id = ""
+
+
 
 const name = 'systar'.toLowerCase().replace(/ /g, '');
 
@@ -12,10 +16,30 @@ router.get('/', function(req,res){
 
 
 router.get('/summonerid', function (req, res){
-    request('https://na.api.riotgames.com/api/lol/NA/v1.4/summoner/by-name/synstar?api_key=' + lolkey, function(err,body){
+    request('https://na.api.riotgames.com/api/lol/NA/v1.4/summoner/by-name/'+ summoner_name + '?api_key=' + lolkey, function(err,body){
        let summoner_id = JSON.parse(body.body)
         res.send(summoner_id)
     })
 })
+
+router.post('/', function (req, res){
+    getid(req.body.summoner_name)
+    res.json(req.body)
+})
+
+function getid(sum_name){
+     request('https://na.api.riotgames.com/api/lol/NA/v1.4/summoner/by-name/'+ sum_name + '?api_key=' + lolkey, function(err,body){
+       let summoner_obj = JSON.parse(body.body)
+       let summoner_id = summoner_obj[sum_name].id
+       unrankedStats(summoner_id)
+    })
+}
+
+function unrankedStats(id){
+    request('https://na.api.riotgames.com/api/lol/NA/v1.3/stats/by-summoner/' + id + '/summary?season=SEASON2017&api_key=' + lolkey, function(err,body){
+        let summoner_obj = JSON.parse(body.body)
+        console.log(summoner_obj.playerStatSummaries)
+    })
+}
 
 module.exports = router;
